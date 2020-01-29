7bb4c88bd0df8941d02219fa47002be6b5bce44b
reguv760 <reguv760@gmail.com>
2020-01-29 12:10:48 -0800

    init repo: connect Gatsby to WP

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import { graphql, StaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -7,14 +8,31 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <StaticQuery
+      query={graphql`
+        {
+          allWordpressPage {
+            edges {
+              node {
+                id
+                title
+                content
+              }
+            }
+          }
+        }
+      `}
+      render={props => (
+        <div>
+          {props.allWordpressPage.edges.map(page => (
+            <div key={page.node.id}>
+              <h1>{page.node.title}</h1>
+              <div dangerouslySetInnerHTML={{ __html: page.node.content }} />
+            </div>
+          ))}
+        </div>
+      )}
+    ></StaticQuery>
   </Layout>
 )
 
